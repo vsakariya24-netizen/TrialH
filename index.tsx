2a9727af05768def5698a29814acd62a8d78518d
vsakariya24-netizen <vsakariya24@gmail.com>
2026-06-26 15:04:50 +0530

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import { Analytics } from '@vercel/analytics/react';
+import './index.css';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+// Only run this code in the browser, not during SSR build
+if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    createRoot(rootElement).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-    <Analytics />   {/* <-- Ye line aapne miss kar di thi, ab add ho gayi */}
-  </React.StrictMode>
-);
